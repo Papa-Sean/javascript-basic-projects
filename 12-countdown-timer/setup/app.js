83cad78ed5f095ac09aed3e8de77eb9b7e267419
Sean Sugrue <seaner@Seans-Mac-mini.local>
2023-05-02 13:36:41 -0400

    countdown timer

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -21,3 +21,69 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+// console.log(items)
+
+let futureDate = new Date(2023,3,6,8,30,0);
+// console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+
+const weekday = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `giveaway ends on ${weekday} ${date}th of ${month} ${year} at ${hours}:${minutes}am`;
+
+//future time in ms
+
+const futureTime = futureDate.getTime();
+// console.log(futureTime);
+
+function getRemainingTime(){
+  const today = new Date().getTime();
+  // console.log(today);
+  const t = futureTime - today;
+  // console.log(t);
+
+  // values in ms
+  const oneDay = 24*60*60*1000;
+  // console.log(oneDay);
+  const oneHour = 60*60*1000;
+  const oneMinute = 60*1000;
+  //calculate all values
+  let days = t/oneDay
+  // console.log(days);
+  days = Math.floor(days);
+  let hours = Math.floor((t% oneDay)/oneHour);
+  let minutes = Math.floor((t% oneHour)/oneMinute);
+  let seconds = Math.floor((t% oneMinute)/1000);
+
+  //set values array
+  const values = [days,hours,minutes,seconds]
+
+  function format(item){
+    if(item <10){
+      return item = `0${item}`
+    }
+    return item
+  }
+  items.forEach(function(item,index){
+    item.innerHTML = format(values[index])
+  })
+  if(t<0){
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class='expired'>sorry, this giveaway has expired</h4>`
+  }
+}
+//contdown
+let countdown = setInterval(getRemainingTime,1000)
+
+getRemainingTime()
