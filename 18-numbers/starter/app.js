2b586ce00b961e9a518bd7e160a81d3e3860c02b
Sean Sugrue <seaner@Seans-Mac-mini.local>
2023-05-13 16:52:01 -0400

    counter complete

--- 18-numbers/starter/app.js
+++ 18-numbers/starter/app.js
@@ -1,1 +1,27 @@
-console.log('numbers project');
+const items = [...document.querySelectorAll('.number')];
+// console.log(items);
+
+const updateCount = (el) =>{
+    // console.log(el);
+    const value = parseInt(el.dataset.value);
+    const increment = Math.ceil(value/1000);
+    let initialValue = 0;
+
+    const increastCount = setInterval(()=>{
+        initialValue += increment;
+
+        if (initialValue > value) {
+            el.textContent = `${value}+`;
+            clearInterval(increastCount);
+            return;
+        }
+
+
+        el.textContent = `${initialValue}+`;
+    },1)
+}
+
+items.forEach((item)=>{
+    updateCount(item);
+});
+
