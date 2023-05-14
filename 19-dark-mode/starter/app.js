10c81383bff13201a45e08b86dece96a9080f311
Sean Sugrue <seaner@Seans-Mac-mini.local>
2023-05-14 16:12:56 -0400

    added css and toggle button for dark theme

--- 19-dark-mode/starter/app.js
+++ 19-dark-mode/starter/app.js
@@ -1,1 +1,8 @@
-console.log('dark mode');
+const toggleBtn = document.querySelector('.btn');
+
+toggleBtn.addEventListener('click',()=>{
+    // console.log('hello');
+    document.documentElement.classList.toggle('dark-theme');
+});
+
+
