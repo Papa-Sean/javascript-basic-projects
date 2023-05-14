78e94ba494e5b8ba83b15ef23d7a5456fd6e6b9d
Sean Sugrue <seaner@Seans-Mac-mini.local>
2023-05-14 16:38:44 -0400

    added moment.js functionality for formatting the date

--- 19-dark-mode/starter/app.js
+++ 19-dark-mode/starter/app.js
@@ -1,8 +1,31 @@
 const toggleBtn = document.querySelector('.btn');
+const articlesContainer = document.querySelector('.articles');
 
 toggleBtn.addEventListener('click',()=>{
     // console.log('hello');
     document.documentElement.classList.toggle('dark-theme');
 });
+
+
+const articlesData = articles.map((article)=>{
+    const{title, date, length, snippet} = article;
+    //format date
+    const formatDate = moment(date).format('MMMM Do, YYYY');
+    return `<article class="post">
+    <h2>${title}</h2>
+    <div class="post-info">
+      <span>${formatDate}</span>
+      <span>${length} min read</span>
+      <p>
+        ${snippet}
+      </p>
+    </div>
+  </article>`
+}).join('');
+
+articlesContainer.innerHTML = articlesData;
+// added a "script" tag in html so data.js is now available
+// console.log(articles);
+// console.log(moment);
 
 
